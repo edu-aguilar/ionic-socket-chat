e82f4e9badc587a759e20fb77fb9c1926928fdde
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2016-07-01 13:56:46 +0200

    added ionic actionsheet to take photo.

--- ionic-socket-chat/www/js/controllers/settings.controller.js
+++ ionic-socket-chat/www/js/controllers/settings.controller.js
@@ -6,12 +6,58 @@
         .controller('SettingsController', SettingsController);
 
     /* @ngInject */
-    function SettingsController($scope, $localStorage) {
+    function SettingsController($scope, $localStorage, $ionicActionSheet, $cordovaCamera) {
         var vm = this;
         vm.selectedColour = $localStorage.backgroundColor;
+
+        //scope methods
+        vm.showActionSheet = showActionSheet;
 
         $scope.$watch('vm.selectedColour', function() {
             $localStorage.backgroundColor = vm.selectedColour;
         });
+
+        function showActionSheet() {
+            var obj = {
+                buttons: [
+                   { text: 'Cámara' },
+                   { text: 'Galería' }
+                ],
+                titleText: 'Seleccione imagen',
+                cancelText: 'Cerrar',
+                cancel: function() {
+                    console.log('actionSheet closed');
+                },
+                buttonClicked: function(index) {
+                    return true;
+                }
+            };
+
+            var backgroundImageActionSheet = $ionicActionSheet.show(obj);
+        }
+
+        //private
+        function takePicture() {
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 100,
+                targetHeight: 100,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false,
+            correctOrientation:true
+            };
+
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+                console.log(imageData);
+                image.src = "data:image/jpeg;base64," + imageData;
+            }, function(err) {
+                // error
+            });
+
+        }
     }
 })();
