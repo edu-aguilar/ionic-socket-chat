c59329e813dff057146dd5c69ce68cb0cc057fe1
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2016-07-04 14:20:21 +0200

    chatMessage factory to save user colour.

--- ionic-socket-chat/www/js/components/chatMessage/chatMessage.component.js
+++ ionic-socket-chat/www/js/components/chatMessage/chatMessage.component.js
@@ -15,9 +15,46 @@
         .module('components')
         .component('chatMessage', chatMessage);
 
-    function chatMessageController() {
+    function chatMessageController(chatMessageData) {
         var vm = this;
         vm.extUser = vm.currentuser !== vm.username;
+        vm.colorName = chatMessageData.getUserColor(vm.username);
     }
 
+    function getUserColor() {
+
+    }
+
+})();
+
+
+(function() {
+    'use strict';
+
+    angular
+        .module('components')
+        .factory('chatMessageData', chatMessageData);
+
+    /* @ngInject */
+    function chatMessageData() {
+        var service = {
+            getUserColor: getUserColor
+        };
+
+        var data = {};
+
+        return service;
+
+        function getUserColor(property) {
+            var color = null;
+            if (data.hasOwnProperty(property)) {
+                color = data[property];
+            } else {
+                var newColor = "#"+((1<<24)*Math.random()|0).toString(16);
+                data[property] = newColor;
+                color = newColor;
+            }
+            return color;
+        }
+    }
 })();
